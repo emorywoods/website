6d7e7f897ea06fdad5d59a01d1e68714cff8283d
Jonathan Sugijono <79694331+ijonathans@users.noreply.github.com>
2026-05-19 16:28:59 -0400

    adjust error in inquiry

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
   webpack: (config) => {
     config.resolve.symlinks = false;
     config.snapshot = {
